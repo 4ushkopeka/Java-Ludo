a73bb1065483e92cfc7bacbd679f52010101a8ae
Georgi Chitarliev <g.chitarliev@gmail.com>
2025-01-07 19:25:03 +0200

    Mild Refactoring

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -37,40 +37,48 @@ public class Game extends GameEngine {
 
        ArrayList<ISymbol> outSymbols = currentPlayer.getSymbolsOutOfBase();
        if (moveNumber < 6) {
-           if (!outSymbols.isEmpty()) {
-               console.print("You have " + outSymbols.size() + " symbol(s) out.");
-               console.print("Their ids are respectively: " + outSymbols.stream().map(ISymbol::getId)
-                       .map(String::valueOf)
-                       .collect(Collectors.joining(", ")) + ".");
-               console.print("Choose the symbol to move:");
-               currentPlayer.move(Integer.parseInt(console.read()));
-           } else {
-               console.print("You have no moves! Moving on.");
-           }
+           this.rolledLessThan6Choices(outSymbols, currentPlayer);
        } else {
            rotate = false;
-           if (outSymbols.size() == 4) {
-               console.print("Choose which symbol to move(Press 1 or 2 or 3 or 4).");
-               currentPlayer.move(Integer.parseInt(console.read()));
-           } else if (!outSymbols.isEmpty()) {
-               console.print("You have symbol(s) out. Press 1 if you want to move on, or 2 to bring out another one.");
-               if (console.read().equals("1")) {
-                   console.print("Choose which symbol to move(Press " + outSymbols.stream().map(ISymbol::getId)
-                           .map(String::valueOf)
-                           .collect(Collectors.joining(", ")) + ".");
-                   currentPlayer.move(Integer.parseInt(console.read()));
-               } else {
-                   currentPlayer.initiate();
-               }
-           } else {
-               currentPlayer.initiate();
-           }
+           this.rolled6Choices(outSymbols, currentPlayer);
        }
        if (rotate) {
            players.add(players.remove(0));
        }
        console.printBoard(board);
        console.print("\n");
+    }
+
+    private void rolled6Choices(ArrayList<ISymbol> outSymbols, IPlayer currentPlayer) {
+        if (outSymbols.size() == 4) {
+            console.print("Choose which symbol to move(Press 1 or 2 or 3 or 4).");
+            currentPlayer.move(Integer.parseInt(console.read()));
+        } else if (!outSymbols.isEmpty()) {
+            console.print("You have symbol(s) out. Press 1 if you want to move on, or 2 to bring out another one.");
+            if (console.read().equals("1")) {
+                console.print("Choose which symbol to move(Press " + outSymbols.stream().map(ISymbol::getId)
+                        .map(String::valueOf)
+                        .collect(Collectors.joining(", ")) + ".");
+                currentPlayer.move(Integer.parseInt(console.read()));
+            } else {
+                currentPlayer.initiate();
+            }
+        } else {
+            currentPlayer.initiate();
+        }
+    }
+
+    private void rolledLessThan6Choices(ArrayList<ISymbol> outSymbols, IPlayer currentPlayer) {
+        if (!outSymbols.isEmpty()) {
+            console.print("You have " + outSymbols.size() + " symbol(s) out.");
+            console.print("Their ids are respectively: " + outSymbols.stream().map(ISymbol::getId)
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(", ")) + ".");
+            console.print("Choose the symbol to move:");
+            currentPlayer.move(Integer.parseInt(console.read()));
+        } else {
+            console.print("You have no moves! Moving on.");
+        }
     }
 
     @Override
